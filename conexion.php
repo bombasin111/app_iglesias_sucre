91f4d22564f3ec017b1a6a067f203c6ae6b90cfb
bombasin111 <bombasin111@gmail.com>
2025-03-01 18:45:40 -0400

    Actualizar conexion.php para usar variables de entorno

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php
 // conexion.php
-$host = 'localhost'; // Host local
-$dbname = 'iglesias_localidad'; // Nombre de la base de datos
-$user = 'cumpa_user'; // Usuario que creaste
-$password = 'Bom111Ba-'; // Contraseña que asignaste al usuario
+$host = getenv('dpg-cv1ok2d6l47c73fi3rn0-a.oregon-postgres.render.com');       // Host de la base de datos
+$dbname = getenv('iglesias_localidad_0nd1');     // Nombre de la base de datos
+$user = getenv('bombasin111');       // Usuario de la base de datos
+$password = getenv('LOGlCiLdaP9T6a5O8PN3QM6A9Er7xul3'); // Contraseña de la base de datos
 
 try {
     $conexion = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
