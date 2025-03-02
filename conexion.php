cfb75dd79d11ead72342c69f40ddb820134d4085
bombasin111 <bombasin111@gmail.com>
2025-03-02 01:23:07 -0400

    Corregir nombre de la base de datos a iglesias_localidad_0nd1

--- conexion.php
+++ conexion.php
@@ -2,7 +2,7 @@
 // conexion.php
 $host = ('dpg-cv1ok2d6l47c73fi3rn0-a.oregon-postgres.render.com');       // Host de la base de datos
 $dbname = ('iglesias_localidad');     // Nombre de la base de datos
-$user = ('bombasin111');       // Usuario de la base de datos
+$user = ('cumpa_user');       // Usuario de la base de datos
 $password = ('LOGlCiLdaP9T6a5O8PN3QM6A9Er7xul3'); // Contraseña de la base de datos
 
 try {
