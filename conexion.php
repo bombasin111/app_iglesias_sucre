0cb8d57488284020844a197ef331f61312fcc3db
bombasin111 <bombasin111@gmail.com>
2025-03-02 01:25:58 -0400

    Corregir nombre de la base de datos a iglesias_localidad_0nd1

--- conexion.php
+++ conexion.php
@@ -3,7 +3,7 @@
 $host = ('dpg-cv1ok2d6l47c73fi3rn0-a.oregon-postgres.render.com');       // Host de la base de datos
 $dbname = ('iglesias_localidad');     // Nombre de la base de datos
 $user = ('cumpa_user');       // Usuario de la base de datos
-$password = ('LOGlCiLdaP9T6a5O8PN3QM6A9Er7xul3'); // Contraseña de la base de datos
+$password = ('BomBa'); // Contraseña de la base de datos
 
 try {
     $conexion = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
