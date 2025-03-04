19fa00b94e7aea008327c4f63b67f09c9d219ea1
bombasin111 <bombasin111@gmail.com>
2025-03-03 23:36:45 -0400

    Habilitar SSL/TLS en la conexión a PostgreSQL

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
 // conexion.php
 $host = ('dpg-cv1ok2d6l47c73fi3rn0-a.oregon-postgres.render.com');       // Host de la base de datos
-$dbname = ('iglesias_localidad_0nd1');     // Nombre de la base de datos
+$dbname = ('iglesias_localidad');     // Nombre de la base de datos
 $user = ('bombasin111');       // Usuario de la base de datos
 $password = ('LOGlCiLdaP9T6a5O8PN3QM6A9Er7xul3'); // Contraseña de la base de datos
 
